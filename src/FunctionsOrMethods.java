62d1c6d81ec40ccbbed2a822f3e73f04abab17b2
yash112005 <ynamdeo248@gmail.com>
2025-04-26 13:27:49 +0530

    java

---
+++ src/FunctionsOrMethods.java
@@ -0,0 +1,55 @@
+public class Functions{
+    // Q1. . Write a program to create a method that prints "Java is Fun!".
+    public static void javaIsFun() {
+        System.out.println("Java is Fun!");
+    }
+
+    // Q2.  Create a method and print your favorite programming language.
+    public static void language() {
+        System.out.println("Python");
+    }
+
+    // Q3  Write a method that prints your name three times using only method calls.
+    public static void name() {
+        System.out.println("John");
+    }
+
+    // Q4 Create a method that prints "Welcome to Java Methods" twice without using
+    //loops.
+    public static void print() {
+        System.out.println("welcome to java method");
+    }
+
+    // Q5. Design a method that prints your first name and last name in two separate
+    //lines.
+    public static void names() {
+        System.out.println( "John");
+        System.out.println( "Doe");
+    }
+
+
+    public static void main(String[] args) {
+
+        javaIsFun(); // Q1
+        language(); // Q2
+        // Q3
+        name();
+        name();
+        name();
+
+        // Q4
+        print();
+        print();
+
+        // Q5.
+        names();
+
+
+    }
+
+
+
+
+
+}
+
