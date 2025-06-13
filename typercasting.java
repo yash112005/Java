35884764bf0d538b66cc0eb9e7648829872a80ea
Yash Namdeo <ynamdeo248@gmail.com>
2025-06-13 19:35:50 +0530

    java

---
+++ typercasting.java
@@ -0,0 +1,15 @@
+
+class casting{
+  public static void main(String[] args) {
+   // casting
+    // implicit casting
+    double price = 110.0;
+    double finalprice = price + 18;
+    System.out.println(finalprice);
+
+    // explicit casting
+    int p  = 100;
+    int fp = p + (int)18.0;
+    System.out.println(fp);
+  }
+}
