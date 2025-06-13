8bc29c99a85954260d7e7bd59643d52af628641d
Yash Namdeo <ynamdeo248@gmail.com>
2025-06-13 19:49:14 +0530

    java

---
+++ inputInJava.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class inputs{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input your age:");
+    int age = sc.nextInt();
+    System.out.println(age);
+
+    // next function only take input of one sentence
+    String name = sc.next();
+    System.out.println(name);
+
+   //  nextline() function takes and print whole line
+    String name1 = sc.nextLine();
+    System.out.println(name1);
+  }
+}
+
+
+public class inputInJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int count = 0;
+        int count1 = 0;
+        for(int i=0;i<s.length();i++){
+
+        }
+        System.out.println(count);
+    }
+}
+
+
+
